cbdb6084ff0275bd547ae7812a9843a85919f85d
Ice322 <killerproffi01@gmail.com>
2026-02-01 05:13:19 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ import os
 # ================= НАСТРОЙКИ =================
 
 TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    raise RuntimeError("TOKEN not found in environment variables")
 THUMBNAIL_URL = "https://media.discordapp.net/attachments/1070143838435422288/1070143912766865499/1b1261398fdfc086.png?ex=697f4bef&is=697dfa6f&hm=a413afa35cc7d39601b902008cf087398ff950844f4eb161d8ef7dfc5164f4f3&=&format=webp&quality=lossless&width=856&height=856"  # ссылка на картинку
 
 # ============================================
